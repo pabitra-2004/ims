f246a3601df2d67c2951d0016d5003619c1ead37
Pabitra Das <pabitra4375@gmail.com>
2026-02-13 20:21:03 +0530

    add new component for product create and edit feature

---
+++ app/Livewire/Products/CreateEditProduct.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Products;
+
+use Livewire\Component;
+
+class CreateEditProduct extends Component
+{
+    public function render()
+    {
+        return view('livewire.products.create-edit-product');
+    }
+}
