db4c2f93de8cd3b3e5fd01af60d0d01f7069f680
Pabitra Das <pabitra4375@gmail.com>
2026-02-18 16:15:10 +0530

    implement create and edit functionality for products

--- app/Livewire/Products/CreateEditProduct.php
+++ app/Livewire/Products/CreateEditProduct.php
@@ -2,10 +2,72 @@
 
 namespace App\Livewire\Products;
 
+use App\Models\Product;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class CreateEditProduct extends Component
 {
+    #[Locked]
+    public $product_id;
+
+    public $code;
+
+    public $name;
+
+    public $slug;
+
+    public ?string $description = null;
+
+    #[On('edit-product')]
+    public function loadProduct(Product $product)
+    {
+        $this->product_id = $product->id;
+        $this->code = $product->code;
+        $this->name = $product->name;
+        $this->slug = $product->slug;
+        $this->description = $product->description;
+
+        $this->modal('create-edit-product')->show();
+    }
+
+    public function updatedName()
+    {
+        $this->slug = $this->slug ?? Str::slug($this->name);
+    }
+
+    public function saveProduct()
+    {
+        $this->slug = Str::trim($this->slug);
+
+        $this->validate([
+            'code' => ['required', 'string', 'max:13', Rule::unique('products', 'code')->ignore($this->product_id)],
+            'name' => ['required', 'string', 'max:255'],
+            'slug' => ['required', 'string', 'max:255', Rule::unique('products', 'slug')->ignore($this->product_id)],
+            'description' => 'nullable|string',
+        ]);
+
+        $product = Product::findOrNew($this->product_id);
+        $product->code = $this->code;
+        $product->name = $this->name;
+        $product->slug = $this->slug;
+        $product->description = $this->description;
+
+        $product->save();
+        $this->dispatch('product-saved');
+        $this->modal('create-edit-product')->close();
+        $this->close();
+    }
+
+    public function close()
+    {
+        $this->reset();
+        $this->resetValidation();
+    }
+
     public function render()
     {
         return view('livewire.products.create-edit-product');
--- resources/views/livewire/products/create-edit-product.blade.php
+++ resources/views/livewire/products/create-edit-product.blade.php
@@ -1,3 +1,28 @@
 <div>
-    {{-- It always seems impossible until it is done. - Nelson Mandela --}}
+    <flux:modal name="create-edit-product"  @close="close"  class="md:w-xl">
+        <form wire:submit="saveProduct" class="space-y-6">
+            <div class="space-y-6">
+                <div>
+                    <flux:heading size="lg">{{ $product_id ?'Update' : 'Create' }} Product</flux:heading>
+                    <flux:text class="mt-2">Enter details to {{ $product_id ? 'update the' : 'add a new' }} product.</flux:text>
+                </div>
+
+                <flux:input type="text" wire:model="code" label="Code" placeholder="Product code"
+                    description="eg: FMP-EMO-2026-305" />
+                <flux:input type="text" wire:model.blur.live="name" label="Name" placeholder="Product name" />
+
+                <flux:input type="text" wire:model="slug" label="Slug" placeholder="Slug" badge="Optional"
+                    description="eg: water-bottle" />
+
+                <flux:textarea wire:model="description" label="Description" placeholder="Enter description here..."
+                    badge="Optional" />
+
+                <div class="flex">
+                    <flux:spacer />
+
+                    <flux:button type="submit" variant="primary">{{ $product_id ?'Save changes' : 'Add product' }}</flux:button>
+                </div>
+            </div>
+        </form>
+    </flux:modal>
 </div>
