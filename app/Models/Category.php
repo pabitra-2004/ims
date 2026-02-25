59a42fdad20faadb4152d908598e2050ad180d53
Pabitra Das <pabitra4375@gmail.com>
2026-02-25 14:23:57 +0530

    define relationship between product and category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -53,5 +54,9 @@ class Category extends Model
                 $filters
             ));
         }
+    }
+
+    public function products(): HasMany{
+        return $this->hasMany(Product::class);
     }
 }
