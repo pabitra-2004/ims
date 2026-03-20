279f57213c6e89ca9cbd0c76957865ed98f544f7
Souvik Das <souvikdas.dev@gmail.com>
2026-03-20 18:04:44 +0530

    added swal-corfirm feature

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,4 +18,53 @@ document.addEventListener("livewire:init", () => {
             },
         });
     });
+
+    Livewire.directive(
+        "swal-confirm",
+        ({ el, directive, component, cleanup }) => {
+            // console.log(el, directive, component, cleanup);
+
+            let expression = directive.expression;
+
+            // The "directive" object gives you access to the parsed directive.
+            // For example, here are its values for: wire:click.prevent="deletePost(1)"
+            //
+            // directive.raw = wire:click.prevent
+            // directive.value = "click"
+            // directive.modifiers = ['prevent']
+            // directive.expression = "deletePost(1)"
+
+            let onClick = (e) => {
+                // console.log(content, JSON.parse(content));
+
+                // string to object convertion
+                const { title, text, action, params, ...config } = new Function(
+                    "return " + expression,
+                )();
+                // console.log(title, text, action, params, config);
+
+                Swal.fire({
+                    title: title ?? "Are you sure?",
+                    text: text,
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Yes, confirm!",
+                    ...config,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        component.$wire.$call(action, ...params);
+                    }
+                });
+            };
+
+            el.addEventListener("click", onClick, { capture: true });
+
+            // Register any cleanup code inside `cleanup()` in the case
+            // where a Livewire component is removed from the DOM while
+            // the page is still active.
+            cleanup(() => {
+                el.removeEventListener("click", onClick);
+            });
+        },
+    );
 });
