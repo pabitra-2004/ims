c4fa66dcf58e2c2ab084824719078161907063ce
Pabitra Das <pabitra4375@gmail.com>
2026-05-23 14:51:14 +0530

    add category filter for order create page

--- app/Livewire/Orders/OrderCreate.php
+++ app/Livewire/Orders/OrderCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Orders;
 
+use App\Models\Category;
 use App\Models\Product;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
@@ -9,17 +10,31 @@ use Livewire\Component;
 class OrderCreate extends Component
 {
     public string $search = '';
+
+    public array $filter = [
+        'categories' => [],
+    ];
+    public $categories = [];
+   
+
     public array $selected_products = [];
 
     public function mount()
     {
-        Product::inRandomOrder()->take(4)->get()->each(fn($product) => $this->addToCart($product));
+        Product::inRandomOrder()->take(4)->get()->each(fn ($product) => $this->addToCart($product));
+
+        $products = Product::select('category_id')->distinct();
+        $this->categories = Category::select('id', 'name')
+            ->whereIn('id', $products)
+            ->orderBy('name')
+            ->get()
+            ->toArray();
     }
 
     #[Computed()]
     public function products()
     {
-        return Product::with(['category:id,name'])->search($this->search)->latest()->get();
+        return Product::with(['category:id,name'])->search($this->search)->filter($this->filter)->latest()->get();
     }
 
     public function addToCart(Product $product)
@@ -28,21 +43,22 @@ class OrderCreate extends Component
 
         $key = array_search($product->id, array_column($this->selected_products, 'id'));
 
-        // dd($key);
-
-        if ($key === false)
+        if ($key === false) {
             $this->selected_products[] = [
                 'id' => $product->id,
                 'name' => $product->name,
                 'price' => $product->price,
-                'image' => $product->images[0],
+                'image' => $product->images[0] ?? null,
                 'category' => $product->category->name ?? 'N/A',
                 'qty' => 1,
             ];
-        else {
-
+        } else {
             $this->selected_products[$key]['qty']++;
         }
+    }
+
+    public function clearFilters(){
+        $this->filter['categories'] = [];
     }
 
     public function render()
